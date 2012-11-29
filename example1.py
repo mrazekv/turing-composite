e2a8860ecc8652f840120fd949b8aff9f151c595
Vojta Mrazek <mrazek.v@gmail.com>
2012-11-29 15:16:45 +0100

    Pridani prikaldu

---
+++ example1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+# data z https://www.fit.vutbr.cz/study/courses/TIN/public/Prednasky/tin-pr07-ts1.pdf
+from ts import *
+ts=TS()
+
+ts.AddCmd([R(), R(), R(), R(), Sr()])
+
+ts.test("______________xyyxx_________")
---
+++ example2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# data z https://www.fit.vutbr.cz/study/courses/TIN/public/Prednasky/tin-pr07-ts1.pdf
+from ts import *
+ts=TS()
+
+# Kopirovaci stroj  _w_ transformuje na _w_w_
+s1=ts.AddCmd([R("_"), R(), C("_"), L("_"), L("_")])
+s2=ts.AddCmd([R()])
+s3=ts.AddCmd([Mem("w"), C("_"), R("_"), R("_"), C(MemRead(ts, "w")), R(), C("_"), L("_"), L("_"), C(MemRead(ts, "w"))])
+
+ts.addCon(s1, s2)
+ts.addCon(s2, s3, "x")
+ts.addCon(s2, s3, "y")
+ts.addCon(s3, s2)
+
+
+ts.test("____xyyxx_______________")
